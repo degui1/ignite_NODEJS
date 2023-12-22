bb8ab4db52ffccaca25406b8f28947336de4d2f5
degui1 <gui.denez56@gmail.com>
2023-12-22 20:02:01 -0300

    Tratativa de erros

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import express, { Express } from "express";
+import "reflect-metadata";
+import express, { Express, NextFunction, Request, Response } from "express";
 import { serve, setup } from "swagger-ui-express";
+import "express-async-errors";
 
 import "./database";
 import "./shared/container";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -12,5 +15,20 @@ const app: Express = express();
 app.use(express.json());
 app.use("/api-docs", serve, setup(swaggerFile));
 app.use(router);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (err: Error, req: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
