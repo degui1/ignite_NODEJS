d009b71a3cd32763d1f1717e0a5bee1fc31c4ddc
degui1 <gui.denez56@gmail.com>
2024-02-18 19:34:27 -0300

    Ajuste os testes do CreateCategoryController

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,26 +1,23 @@
 import { hash } from "bcryptjs";
 import request from "supertest";
 import { Connection } from "typeorm";
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuid } from "uuid";
 
 import { app } from "@shared/infra/http/app";
 import createConnection from "@shared/infra/typeorm";
 
 let connection: Connection;
-
-describe("Create category controller", () => {
+describe("Create Category Controller", () => {
   beforeAll(async () => {
-    connection = await createConnection("localhost");
-    const test = await connection.runMigrations();
+    connection = await createConnection();
+    await connection.runMigrations();
 
-    const id = uuidV4();
+    const id = uuid();
     const password = await hash("admin", 8);
 
-    console.log(test);
-
     await connection.query(
-      `INSERT INTO users(id, name, email, password, "isAdmin", created_at, driver_license)
-      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', 'true', 'now()', 'xxxxxx')
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license ) 
+        values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
       `,
     );
   });
@@ -30,7 +27,7 @@ describe("Create category controller", () => {
     await connection.close();
   });
 
-  it("Should be able to create a new category", async () => {
+  it("should be able to create a new category ", async () => {
     const responseToken = await request(app).post("/sessions").send({
       email: "admin@rentx.com.br",
       password: "admin",
@@ -49,5 +46,26 @@ describe("Create category controller", () => {
       });
 
     expect(response.status).toBe(201);
+  });
+
+  it("Should not be able to create a new category with name exists", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body.token;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Category Supertest",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(400);
   });
 });
